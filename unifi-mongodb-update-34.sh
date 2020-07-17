30ce74ca8d4193b5629b27755169731309ac4aab
Reilly Chase <rchase@rchase.com>
2020-07-17 11:40:35 -0400

    Update unifi-mongodb-update-34.sh

--- unifi-mongodb-update-34.sh
+++ unifi-mongodb-update-34.sh
@@ -5,6 +5,7 @@ wget -O /root/libssl1.0.0_1.0.1t-1+deb8u12_amd64.deb http://security.debian.org/
 dpkg -i /root/libssl1.0.0_1.0.1t-1+deb8u12_amd64.deb
 wget -qO - https://www.mongodb.org/static/pgp/server-3.4.asc | apt-key add -
 echo "deb http://repo.mongodb.org/apt/debian jessie/mongodb-org/3.4 main" | tee /etc/apt/sources.list.d/mongodb-org-3.4.list
+apt-get update -y
 apt-get install -y mongodb-org
 apt-get install -y mongodb-org=3.4.23 mongodb-org-server=3.4.23 mongodb-org-shell=3.4.23 mongodb-org-mongos=3.4.23 mongodb-org-tools=3.4.23
 dpkg -i --force-overwrite /var/cache/apt/archives/mongodb-org-tools_3.4.24_amd64.deb
